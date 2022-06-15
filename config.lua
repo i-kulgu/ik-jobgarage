f051deba36506c9f960376690468ac9ce4c85988
Ilker Kulgu <i.kulgu@gmail.com>
2022-06-16 00:03:09 +0200

    fuel and plate system

--- config.lua
+++ config.lua
@@ -5,6 +5,8 @@ Config.UseCarItems = false -- Change into true if you want to use trunk items. L
 Config.CustomExtras = false -- Set this true if you want custom car extra's. Look down for Config.CarExtras to set up extras
 Config.CustomLivery = false -- Set this true if you want custom livery for your cars. Look down for Config.CarExtras to set up livery
 Config.MaxMod = false -- Set this to true if you want your vehicle max modded. Look down for Config.CarMods to enable / disable the desired parts to max mod
+Config.fuelsystem = "ps-fuel" -- Set your fuel system, LegacyFuel, ps-fuel or other fuel system
+Config.plateprefix = "POLI" -- Set plate prefix, max 4 letters
 Config.viewcoords = vector3(454.75, -1020.45, 28.32)
 
 Config.spawnloc = {
